bba86ecf873f7b4044e2defa26963e989708aee8
Filippo Daniel <work.danielfilippo@gmail.com>
2018-03-17 09:55:23 +0100

    Notebook QLearning

--- examples/QL_frozen.py
+++ examples/QL_frozen.py
@@ -4,7 +4,21 @@ import os
 
 import numpy as np
 
-gamma = 0.9
+
+"""
+SFFF
+FHFH
+FFFH
+HFFG
+[0, x, x, x, 0, x, 1, x, 2, 1, 0, x, x, 3, 1, x]  <- our gamma .9
+[0, 3, 3, 3, 0, 3, 0, 1, 3, 1, 0, 0, 2, 2, 1, 1]  <- GA best
+[3, 3, 1, 1, x, x, 1, x, 1, 1, 1, x, x, 2, 1, x]  <- gamma 0.2
+[1, 1, 0, x, 2, x, 1, x, 2, 1, 0, x, x, 3, 2, x]  <- gamma 1.0
+[1, 1, 0, x, 0, x, 0, x, 3, 2, 1, x, x, 0, 2, x]  <- gamma .5
+
+"""
+
+gamma = 0.2
 alpha = 0.5
 
 def updateQ(Q, s_t, s_tn, a, R):
@@ -29,7 +43,7 @@ def choose_policy(states):
     return indici[0]
 
 
-Q = np.zeros((16, 4))
+Q = np.ones((16, 4))
 
 print("Q(t:0)")
 print(Q)
@@ -40,7 +54,7 @@ acc = 0;
 from gym import wrappers
 env = gym.make('FrozenLake-v0')
 env = wrappers.Monitor(env, '/tmp/frozenlake-experiment-1', force=True)
-for i_episode in range(1000000):
+for i_episode in range(100000):
     s_old = env.reset()
 
     for t in range(100):
