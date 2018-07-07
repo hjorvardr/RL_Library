ce85c8aa62dbee0bde6a7845338397f5a5815d48
Gianluca Bigaglia <bigagliagianluca@gmail.com>
2018-07-07 15:16:03 +0200

    Add FrozenLake with new OO Q-Learning and SARSA, conversion from jupyter to pure python.
    All jupyter unused files have been removed.

---
+++ FrozenLake/ql_4x4.py
@@ -0,0 +1,64 @@
+import time
+import numpy as np
+import gym
+from tqdm import tqdm
+from qlearning_lib import QLAgent
+
+
+def accuracy(results):
+    """
+    Evaluate the accuracy of results, considering victories and defeats.
+    """
+    return results[1] / (results[0] + results[1]) * 100
+
+
+def experiment(n_episodes, max_action, default_policy=False, policy=None, render=False):
+    res = [0, 0] # array of results accumulator: {[0]: Loss, [1]: Victory}
+    scores = [] # Cumulative rewards
+    steps = [] # Steps per episode
+
+    env = gym.make('FrozenLake-v0')
+    env.seed(91)
+    
+    if (default_policy):
+        agent = QLAgent(env.observation_space.n, env.action_space.n, policy=policy)
+    else:
+        agent = QLAgent(env.observation_space.n, env.action_space.n)
+
+    for i_episode in tqdm(range(n_episodes)):
+        state = env.reset()
+        cumulative_reward = 0
+        
+        for t in range(max_action):
+            if (render):
+                env.render()
+                time.sleep(1)
+
+            next_action = agent.act(state, i_episode)
+            new_state, reward, end, _ = env.step(next_action)
+            agent.update_q(state, new_state, next_action, reward)
+            if end:
+                res[int(reward)] += 1
+                steps.append(t)
+                cumulative_reward += reward
+                scores.append(cumulative_reward)
+                break
+            else:
+                state = new_state
+                cumulative_reward += reward
+
+    env.close()
+    return {"results": np.array(res), "steps": np.array(steps), "scores": np.array(scores), "Q": agent.Q}
+
+
+# Training
+res = experiment(50000, 100)
+learnt_policy = np.argmax(res["Q"], axis=1)
+print("Policy learnt: ", learnt_policy)
+
+# Testing
+res2 = experiment(5000, 1000, default_policy=True, policy=learnt_policy)
+print("Testing accuracy: %s, Training mean score: %s" % (accuracy(res2["results"]), np.mean(res["scores"])))
+
+# Rendering
+#experiment(5, 1000, default_policy=True, policy=learnt_policy, render=True)
---
+++ FrozenLake/sarsa_8x8_deterministic.py
@@ -0,0 +1,71 @@
+import time
+import numpy as np
+import gym
+from tqdm import tqdm
+from sarsa_lib import SARSAAgent
+
+
+def accuracy(results):
+    """
+    Evaluate the accuracy of results, considering victories and defeats.
+    """
+    return results[1] / (results[0] + results[1]) * 100
+
+
+def experiment(n_episodes, max_action, default_policy=False, policy=None, render=False):
+    res = [0, 0] # array of results accumulator: {[0]: Loss, [1]: Victory}
+    scores = [] # Cumulative rewards
+    steps = [] # Steps per episode
+
+    env = gym.make('FrozenLakeNotSlippery8x8-v0')
+    env.seed(91)
+    
+    if (default_policy):
+        agent = SARSAAgent(env.observation_space.n, env.action_space.n, policy=policy)
+    else:
+        agent = SARSAAgent(env.observation_space.n, env.action_space.n)
+
+    for i_episode in tqdm(range(n_episodes)):
+        state = env.reset()
+        cumulative_reward = 0
+        agent.update_policy()
+        
+        for t in range(max_action):
+            if (render):
+                env.render()
+                time.sleep(1)
+
+            next_action = agent.act(state, i_episode)
+            new_state, reward, end, _ = env.step(next_action)
+            agent.update_q(state, new_state, next_action, reward)
+            if end:
+                res[int(reward)] += 1
+                steps.append(t)
+                cumulative_reward += reward
+                scores.append(cumulative_reward)
+                break
+            else:
+                state = new_state
+                cumulative_reward += reward
+
+    env.close()
+    return {"results": np.array(res), "steps": np.array(steps), "scores": np.array(scores), "Q": agent.Q}
+
+
+gym.envs.registration.register(
+    id='FrozenLakeNotSlippery8x8-v0',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={'map_name': '8x8', 'is_slippery': False},
+)
+
+# Training
+res = experiment(10000, 1000)
+learnt_policy = np.argmax(res["Q"], axis=1)
+print("Policy learnt: ", learnt_policy)
+
+# Testing
+res2 = experiment(5000, 1000, default_policy=True, policy=learnt_policy)
+print("Testing accuracy: %s, Training mean score: %s" % (accuracy(res2["results"]), np.mean(res["scores"])))
+
+# Rendering
+#experiment(5, 1000, default_policy=True, policy=learnt_policy, render=True)
